65a5f2694c7e79151c1763967deba10d98e7f722
GabrielSB22 <gabrielkachan8@gmail.com>
2025-11-28 14:44:31 +0000

    enlouqueci

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -1,103 +1,81 @@
-class Telefone:
-    def __init__(self, id: str, number: str):
-        self.id = id
-        self.number = number
+from operator import index
 
-    def getId(self):
-        return self.id
+class Telefone:
+    def __init__(self, id: str, numero: str):
+        self.__id = id
+        self.__numero = numero
+
+    def getid(self):
+        return self.__id
+    def getnumero(self):
+        return self.__numero
     
-    def getNumber(self):
-        return self.number
+    def evalido(self) -> bool:
+        g = "0123456789()."
+        return all(c in g for c in self.__numero)
     
     def __str__(self):
-        return f"{self.id}:{self.number}"
+        return f"{self.__id}:{self.__numero}"
 
-    def isvalido(self) -> bool:
-        if not self.id or not self.number:
-            return False
-        if any(c.isalpha() for c in self.number):
-            return False
-        return True
 
 class Contato:
     def __init__(self, name: str):
-        self.name = name
-        self.fone: list[Telefone] = []
-        self.fav: bool = False
+        self.__nome = name
+        self.__fone: list[Telefone] = []
+        self.__favoritado: bool = False
 
-    def getName(self):
-        return self.name
-    
-    def getFone(self):
-        return self.fone
-    
-    def setName(self, name: str):
+    def getnome(self):
+        return self.__nome
+    def setnome(self, name: str):
         self.name = name
+
+    def getfone(self) -> list[Telefone]:
+        return self.__fone
+    
+    def favoritado(self):
+        return self.__favoritado
+
+    def adicionarfone(self, id: str, numero: str):
+        cell = Telefone(id, numero)
+        if cell.evalido():
+            self.__fone.append(cell)
+        else:
+            print("fail: number invalid")
 
     def __str__(self):
-        fone = ", ".join([str(x)for x in self.fone])
-        if self.fav is True:
-            return f"@ {self.name} [{fone}]"
-        else:
-            return f"- {self.name} [{fone}]"
-        
-    def adfone(self, id: str, number: str):
-        fone = Telefone(id, number)
-        if not fone.isvalido():
-            print("fail: invalid number")
-            return
-        self.fone.append(fone)
+        j = "@" if self.__favoritado else "-"
+        nome_ctt = ", ".join(str(cell)fpr cell in self.__fone)
+        return f"{j} {self.__nome} [{nome_ctt}]"
 
-    def removerfone(self, index: int):
-        self.fone.pop(index)
 
-    def colocarfavorito(self):
-        if self.fav == False:
-            self.fav = True
-            return
-        else:
-            self.fav = False
-
-    def tafavoritado(self) -> bool:
-        if self.fav == True:
-            return True
-        else:
-            return False
-        
 class Agenda:
     def __init__(self):
         self.__contatos: list[Contato] = []
 
-    def __str__(self):
-        order = sorted(self.__contatos, key=lambda c: c.getName().lower())
-        return "\n".join(str(contato) for contato in order)
-
-    def getcontatos(self)  -> list[Contato | None]:
+    def getcontato(self):
         return self.__contatos
-    
-    def acharnome(self, name: str):
+
+    def acharpornome(self, name: str):
         name = name.lower()
         for i in range(len(self.__contatos)):
-            if self.__contatos[i].getName().lower() == name:
+            if self.__contatos[i].getnome().lower() == name:
                 return i
         return -1
     
-    def getcontato(self, name: str):
-        pos = self.acharnome(name)
-        if pos == 1:
-            return None
-        return self.__contatos[pos]
-    
     def addcontato(self, name: str):
-        if self.acharnome(name) != -1:
-            print("fail: contato ja existe")
+         if self.acharpornome(name) != -1:
+            print("fail: contato já existe")
             return
+
         self.__contatos.append(Contato(name))
     
-
-
+    def __str__(self):
+        order = sorted(self.__contatos, key= lambda  c: c.getnome().lower())
+        return "\n".join(str(contato) for contato in order)
+    
 def main():
-    agenda = Agenda("")
+    agenda = Agenda()
+
     while True:
         line = input()
         print("$" + line)
@@ -105,17 +83,17 @@ def main():
 
         if args[0] == "end":
             break
-        elif args[0] == "show":
+        elif args[0] == "break":
             print(agenda)
-        elif args[0] == "init":
-            name = args[1]
-            agenda.setName(name)
         elif args[0] == "add":
             name = args[1]
-            contato = agd
-        elif args[0] == "rm":
-            index = int(args[1])
-            contact.removerfone(index)
-        elif args[0] == "tfav":
-            contact.colocarfavorito()
+            contato = agenda.getcontato(name)
+            if contato is None:
+                agenda.addcontato(name)
+                contato = agenda.getcontato(name)
+
+            for token in args[2]:
+                op, num = token.split(":")
+                contato.adicionarfone(op, num)
 main()
+
